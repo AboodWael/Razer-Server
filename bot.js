4c59f2da7c185ace6c095713dc28564b296bc4a2
AboodWael <53835494+AboodWael@users.noreply.github.com>
2019-08-06 22:18:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ m.sendMessage(args)
    }
    });
 
-const developers = ["474496300235227137"]
+const developers = ["571143079013384192"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
